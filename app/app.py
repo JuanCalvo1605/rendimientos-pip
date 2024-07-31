1c3df70947421fa8713fe5a3deaadbb10344bd5d
JuanCalvo1605 <jsebascc2003@gmail.com>
2024-07-31 12:40:35 -0500

    se agrega neuva version

--- app/app.py
+++ app/app.py
@@ -5,11 +5,19 @@ from waitress import serve
 app = Flask(__name__)
 CORS(app)
 
+conn = pymssql.connect(
+    server = '172.20.10.2',
+    user= 'consulta',
+    password= 'Consulta_2017',
+    database = 'MONVER',
+    as_dict= True
+    )
+#cursor = conn.cursor()
+
 # Ruta para obtener datos desde la base de datos
-#@app.route('/obtener_datos', methods=['GET'])
-def obtener_datos(parametro1,parametro2):
+@app.route('/obtener_datos', methods=['GET'])
+def obtener_datos():
     try:
-        print("Intentando conectar a la base de datos...")
         conn = pymssql.connect(
             server = '172.20.10.2',
             user= 'consulta',
@@ -17,25 +25,20 @@ def obtener_datos(parametro1,parametro2):
             database = 'MONVER',
             as_dict= True
             )
-        print("Conexión establecida.")
         cursor = conn.cursor()
-        print(f"Ejecutando consulta con parametros: {parametro1}, {parametro2}")
         # Obtener parámetros de la solicitud
-        #parametro1 = request.args.get('parametro1')
-        #parametro2 = request.args.get('parametro2')
+        parametro1 = request.args.get('parametro1')
+        parametro2 = request.args.get('parametro2')
         # Utilizar parámetros nombrados con SQL Server
         #sql = 
         cursor.execute("SELECT ltrim(rtrim(NOMBRE)) as NOMBRE, REND_HR FROM dbo.fp_inf_rendimiento_clav2(%s, %s)", (parametro1, parametro2))
         resultados = cursor.fetchall()
-        print("Consulta ejecutada exitosamente.")
-        print("Resultados obtenidos:", resultados)
         #resultados_json = []
         #for x in resultados:
             
             #resultados_json.append(dict(zip([column[0] for column in cursor.description], x)))
         # Cerrar la conexión
         conn.close() 
-        print("Conexión cerrada.")
         # Devolver resultados en formato JSON
         #return resultados
         return jsonify(resultados)
@@ -44,5 +47,4 @@ def obtener_datos(parametro1,parametro2):
         return jsonify({'error': str(e)})
     
 if __name__ == '__main__':
-    #serve(app,host = '172.19.10.8',port="8084", threads=2)
-    obtener_datos('20240729','20240729')
+    serve(app,host = '172.90.10.32',port="8087", threads=2)
